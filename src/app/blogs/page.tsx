19b9226cd4afc8afaa9691b394e6b496f90e77c1
phuchoang2005 <lephuc260205@gmail.com>
2026-09-22 10:19:57 +0700

    feat: add Menu and switch content between them

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+  return (
+    <>
+      <p>I am blog</p>
+    </>
+  )
+}
---
+++ src/app/users/page.tsx
@@ -0,0 +1,7 @@
+export default function User() {
+  return (
+    <>
+      <p>I am iron man</p>
+    </>
+  )
+}
